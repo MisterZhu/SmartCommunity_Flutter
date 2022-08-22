16c4b462bcf628908daff3e4f66c368942b572a9
mjl <2994355324@qq.com>
2022-08-22 15:09:47 +0800

    新增加载弹窗

--- lib/utils/Loading/sc_loading_utils.dart
+++ lib/utils/Loading/sc_loading_utils.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 /// 加载显示工具类
@@ -7,8 +9,10 @@ class SCLoadingUtils {
   /// 显示加载框
   static show({String? text}) {
     if (text == null) {
+      EasyLoading.instance.contentPadding = const EdgeInsets.all(20.0);
       EasyLoading.show();
     } else {
+      EasyLoading.instance.contentPadding = const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 15);
       EasyLoading.show(status: text);
     }
   }
@@ -36,9 +40,19 @@ class SCLoadingUtils {
   /// 初始化loading配置
   static initLoading() {
     EasyLoading.instance.toastPosition = EasyLoadingToastPosition.center;
-    EasyLoading.instance.loadingStyle = EasyLoadingStyle.dark;
+    EasyLoading.instance.loadingStyle = EasyLoadingStyle.custom;
     EasyLoading.instance.maskType = EasyLoadingMaskType.clear;
     EasyLoading.instance.animationStyle = EasyLoadingAnimationStyle.opacity;
-    EasyLoading.instance.indicatorType = EasyLoadingIndicatorType.circle;
+    EasyLoading.instance.indicatorType = EasyLoadingIndicatorType.squareCircle;
+    EasyLoading.instance.dismissOnTap = true;
+    EasyLoading.instance.backgroundColor = Colors.black54.withOpacity(0.5);
+    EasyLoading.instance.indicatorColor = Colors.white;
+    EasyLoading.instance.textColor = Colors.white;
+    EasyLoading.instance.indicatorSize = 0.0;
+    EasyLoading.instance.contentPadding = const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 15);
+    EasyLoading.instance.indicatorWidget = const CupertinoActivityIndicator(
+      radius: 16.0,
+      color: Colors.white,
+    );
   }
 }
