9812adcc96236e37dcb731a66b00a4b8585ffe39
ly <lvying@lvchengfuwu.com>
2022-10-21 10:40:20 +0800

    退出登录接口

--- lib/page/Mine/GetXController/sc_setting_controller.dart
+++ lib/page/Mine/GetXController/sc_setting_controller.dart
@@ -1,6 +1,11 @@
 
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
+import '../../../network/sc_http_manager.dart';
+import '../../../network/sc_url.dart';
+import '../../../utils/Loading/sc_loading_utils.dart';
+import '../../../utils/Toast/sc_toast.dart';
+
 class SCSettingController extends GetxController {
 
   bool switchStatus1 = false;
@@ -17,9 +22,29 @@ class SCSettingController extends GetxController {
 
   }
 
-
+  /// 更新开关的状态
   updateSwitchStatus({required int index ,required bool enable}) {
     switchStatusList[index] = enable;
     update();
   }
+
+  /// 退出登录
+  logout({required Function(bool success) resultHandler}) {
+    SCLoadingUtils.show();
+    SCHttpManager.instance.delete(
+        url: SCUrl.kLogoutUrl,
+        params: null,
+        success: (value) {
+          SCLoadingUtils.success(text:'退出登录成功');
+          resultHandler(true);
+        },
+        failure: (value) {
+          if (value['message'] != null) {
+            String message = value['message'];
+            SCToast.showTip(message);
+          }
+          resultHandler(false);
+        });
+  }
+
 }
